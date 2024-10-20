6ec3d52b61045a04efa4569f945f38ca966b6ec5
Meo0432004 <meocho0432004@gmail.com>
2024-10-20 17:46:22 +0700

    a

---
+++ ASPConnect2MySQL/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using ASPConnect2MySQL.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASPConnect2MySQL.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASPConnect2MySQL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPConnect2MySQL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
